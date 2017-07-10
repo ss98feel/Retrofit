df05653d21767c0ac2473f11b9be142c78ca2dcf
dongkibae <ss98feel>
2017-07-10 14:36:11 +0900

    Rejected commit: Default

--- app/src/main/java/com/example/dongkibae/retrofit/MainActivity.java
+++ app/src/main/java/com/example/dongkibae/retrofit/MainActivity.java
@@ -1,15 +1,32 @@
 package com.example.dongkibae.retrofit;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Currency;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -18,70 +35,115 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
-    private Retrofit retrofit;
-    private TextView textView;
+    /**
+     * Views
+     */
+    private ListView listView;
+    private View parentView;
 
-
-    private final String BASE_URL = "https://api.github.com";
-
-
+    private ArrayList<Contact> contactList;
+    private MyContactAdapter adapter;
+    ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        init();
-// GitHub API 인터페이스 생성
-        GitHubService gitHub = retrofit.create(GitHubService.class);
-        // 인터페이스에 구현한 메소드인 contributors에 param 값을 넘기는 요청 만ㄷ름
-        Call<List<Contributor>> call = gitHub.contributors("square", "retrofit");
 
-        // 앞서만든 요청을 수행
-        call.enqueue(new Callback<List<Contributor>>() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        /**
+         * Array List for Binding Data from JSON to this List
+         */
+        contactList = new ArrayList<>();
+
+        parentView = findViewById(R.id.parentLayout);
+
+        /**
+         * Getting List and Setting List Adapter
+         */
+        listView = (ListView) findViewById(R.id.listView);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            // 성공시
-            public void onResponse(Call<List<Contributor>> call, Response<List<Contributor>> response) {
-                List<Contributor> contributors = response.body();
-                // 받아온 리스트를 순회하면서
-                for (Contributor contributor : contributors) {
-                    // 텍스트 뷰에 login 정보를 붙임
-                    textView.append(contributor.login);
-
-                }
-
-
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Snackbar.make(parentView, contactList.get(position).getName() + " => " + contactList.get(position).getPhone().getHome(), Snackbar.LENGTH_LONG).show();
             }
+        });
 
+        /**
+         * Just to know onClick and Printing Hello Toast in Center.
+         */
+        Toast toast = Toast.makeText(getApplicationContext(), R.string.string_click_to_load, Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.show();
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        assert fab != null;
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            // 실패시
-            public void onFailure(Call<List<Contributor>> call, Throwable t) {
-                Toast.makeText(MainActivity.this, "정보받아오기 실패", Toast.LENGTH_LONG)
-                        .show();
+            public void onClick(@NonNull final View view) {
+
+                /**
+                 * Checking Internet Connection
+                 */
+                if (cm.getActiveNetworkInfo() != null) {
+                    if (InternetConnection.checkConnection(getApplicationContext())) {
+                        final ProgressDialog dialog;
+                        /**
+                         * Progress Dialog for User Interaction
+                         */
+                        dialog = new ProgressDialog(MainActivity.this);
+                        dialog.setTitle(getString(R.string.string_getting_json_title));
+                        dialog.setMessage(getString(R.string.string_getting_json_message));
+                        dialog.show();
+
+                        //Creating an object of our api interface
+                        GitHubService api = RetroClient.getApiService();
+
+                        /**
+                         * Calling JSON
+                         */
+                        Call<Contributor> call = api.getMyJSON();
+
+                        /**
+                         * Enqueue Callback will be call when get response...
+                         */
+                        call.enqueue(new Callback<Contributor>() {
+                            @Override
+                            public void onResponse(Call<Contributor> call, Response<Contributor> response) {
+                                //Dismiss Dialog
+                                dialog.dismiss();
+
+                                if (response.isSuccessful()) {
+                                    /**
+                                     * Got Successfully
+                                     */
+                                    contactList = response.body().getContacts();
+
+                                    /**
+                                     * Binding that List to Adapter
+                                     */
+                                    adapter = new MyContactAdapter(MainActivity.this, contactList);
+                                    listView.setAdapter(adapter);
+
+                                } else {
+                                    Snackbar.make(parentView, R.string.string_some_thing_wrong, Snackbar.LENGTH_LONG).show();
+                                }
+                            }
+
+                            @Override
+                            public void onFailure(Call<Contributor> call, Throwable t) {
+                                dialog.dismiss();
+                            }
+                        });
+
+                    } else {
+                        Snackbar.make(parentView, R.string.string_internet_connection_not_available, Snackbar.LENGTH_LONG).show();
+                    }
+                }
             }
         });
     }
-    public void init() {
-        textView = (TextView) findViewById(R.id.textView);
-        // GSON 컨버터를 사용하는 REST 어댑터 생성
-        retrofit = new Retrofit.Builder()
-            .baseUrl(BASE_URL)
-    .addConverterFactory(GsonConverterFactory.create())
-            .build();
-}
-
-
-
-//        Button button = (Button) findViewById(R.id.button);
-//        button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                GitHubService gitHubService = GitHubService.retrofit.create(GitHubService.class);
-//                final Call<List<Contributor>> call = gitHubService.repoContributors("square", "retrofit");
-//                new NetworkCall().execute(call);
-//
-//            }
-//        });
-
-
 
 }
